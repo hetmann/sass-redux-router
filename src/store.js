8ae5e651960d0e19f5d6334acd3cfd37360fe11e
hetmann <contact@blogtools.ro>
2017-03-23 12:43:39 +0200

    Added examples + React Router 4 support

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import Immutable from 'immutable';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from './reducers';
+
+const loggerMiddleware = createLogger({
+  stateTransformer: (state) => {
+    const newState = {};
+
+    for (const i of Object.keys(state)) {
+      if (Immutable.Iterable.isIterable(state[i])) {
+        newState[i] = state[i].toJS();
+      } else {
+        newState[i] = state[i];
+      }
+    }
+
+    return newState;
+  }
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
+);
+
+export default store;
