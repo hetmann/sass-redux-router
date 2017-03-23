519fdf0a68ffddfe5fe46562766cbb076614dfb2
hetmann <contact@blogtools.ro>
2017-03-23 12:43:39 +0200

    Added examples + React Router 4 support

--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
 import './style.scss';
-import Immutable from 'immutable';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
-import App from './containers/app';
-import rootReducer from './reducers';
+
+import { BrowserRouter as Router } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-const loggerMiddleware = createLogger({
-  stateTransformer: (state) => {
-    const newState = {};
+import store from './store';
 
-    for (const i of Object.keys(state)) {
-      if (Immutable.Iterable.isIterable(state[i])) {
-        newState[i] = state[i].toJS();
-      } else {
-        newState[i] = state[i];
-      }
-    }
+import GitHubExplorer from '../examples/github-explorer';
 
-    return newState;
+export default class Wrapper extends React.PureComponent {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          {this.props.children}
+        </Router>
+      </Provider>
+    );
   }
-});
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
-);
+}
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'));
-
+  <Wrapper>
+    <GitHubExplorer />
+  </Wrapper>,
+  document.getElementById('root')
+);
