1c7116f1841baf460a619cbbb476da92c073e178
hetmann <contact@blogtools.ro>
2017-03-24 12:59:26 +0200

    cross-env for development/production

---
+++ examples/github-explorer/modules/index.js
@@ -0,0 +1,7 @@
+import user from './user-management/reducers';
+import repos from './repos/reducers';
+
+export default {
+  user: user,
+  repos: repos
+}
--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,3 @@
 import { combineReducers } from 'redux';
-import user from './modules/user-management/reducers';
-import repos from './modules/repos/reducers';
 
-export default combineReducers({
-  userInformation: user.userInformation,
-  repos: repos.repos
-});
+export default (reducers) => combineReducers(reducers);
